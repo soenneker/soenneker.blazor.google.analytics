00dbee7e364cf0e1ee07bfa2863e23e1259f703c
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:00 -0500

    xml commenting

--- src/Soenneker.Blazor.Google.Analytics/Abstract/IGoogleAnalyticsInterop.cs
+++ src/Soenneker.Blazor.Google.Analytics/Abstract/IGoogleAnalyticsInterop.cs
@@ -9,5 +9,12 @@ namespace Soenneker.Blazor.Google.Analytics.Abstract;
 /// </summary>
 public interface IGoogleAnalyticsInterop : IAsyncDisposable
 {
+    /// <summary>
+    /// Initializes the instance.
+    /// </summary>
+    /// <param name="tagId">The tag id.</param>
+    /// <param name="log">The log.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     ValueTask Init(string tagId, bool log = false, CancellationToken cancellationToken = default);
 }
